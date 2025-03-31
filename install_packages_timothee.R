cc31fcaf5d4204ef7ec524059f38fa7abeedd6b6
Timothée Bacri <t.r.f.bacri@exeter.ac.uk>
2025-03-31 22:54:40 +0000

    stop loading packages on build

--- install_packages_timothee.R
+++ install_packages_timothee.R
@@ -70,9 +70,11 @@ packages <- c("adaptMCMC",
               "viridis",
               "webshot",
               "zipfR")
-available <- sapply(packages, require, character.only = TRUE)
-if (length(packages[!available] > 0)) {
-  pak::pkg_install(packages[!available],
+to_install <- sapply(packages, function(x) {
+  return(system.file(package = x) == ""))
+})
+if (length(packages[to_install] > 0)) {
+  pak::pkg_install(packages[to_install],
                    ask = FALSE,
                    upgrade = FALSE)
 }
